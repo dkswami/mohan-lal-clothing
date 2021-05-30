2471b05a8047d1344678eb0e1c4fcf09470d4624
dkswami <dkswami1820@gmail.com>
2021-05-30 14:28:37 +0530

    using route and switch components for the first time

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
-
+import { Route, Switch } from 'react-router-dom';
 import HomePage from '../src/Pages/homepage/homepage.component';
+
+const HatsPage = () => (
+	<div>
+		<h2>This is HatsPage functional component</h2>
+	</div>
+);
 
 function App() {
   return (
     	<div>
-		<HomePage/>
+		    <Switch>
+				<Route exact path='/' component={ HomePage } />
+				<Route exact path='/hats' component={ HatsPage } />
+		    </Switch>
+		
 	</div>
   );
 }
